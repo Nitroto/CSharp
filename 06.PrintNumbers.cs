b2b1dec3801a55aae0fc4d134374af25296f563b
Nitroto <astodorov@outlook.com>
2015-03-10 15:17:36 +0200

    Create 06.PrintNumbers.cs

---
+++ 06.PrintNumbers.cs
@@ -0,0 +1,11 @@
+using System;
+class PrintNumbers
+{
+    static void Main()
+    {
+        System.Console.WriteLine(1);
+        System.Console.WriteLine(101);
+        System.Console.WriteLine(1001);
+        Console.ReadLine();
+    }
+}
