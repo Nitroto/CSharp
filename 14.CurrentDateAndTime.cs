2f7d74c86df290d4558a4b902745960e53611c07
Nitroto <astodorov@outlook.com>
2015-03-10 15:21:09 +0200

    Create 14.CurrentDateAndTime.cs

---
+++ 14.CurrentDateAndTime.cs
@@ -0,0 +1,11 @@
+using System;
+
+class CurrentDateAndTime
+{
+    static void Main()
+    {
+        DateTime now = DateTime.Now;
+        Console.WriteLine(now);
+        Console.ReadLine();
+    }
+}
